449553a1349a4ed448de2b48dc216237170d525c
Navaneet <navaneet.vinno@gmail.com>
2022-09-16 18:32:21 +0530

    adding table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes } from '@angular/router';
+import { SharedService } from '../app.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 stepper: string|any[]|null|undefined;
 
-  constructor() { }
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit(): void {
   }
